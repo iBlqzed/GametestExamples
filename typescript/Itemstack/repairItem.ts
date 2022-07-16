c91b01c02f3e608a33d9f2b910005e8b2a48766e
iBlqzed <iBlqzed@users.noreply.github.com>
2022-07-16 12:17:31 -0500

    Repair item
    Added repair item function

--- typescript/Itemstack/repairItem.ts
+++ typescript/Itemstack/repairItem.ts
@@ -5,7 +5,7 @@ import { Items, ItemStack } from "mojang-minecraft"
  * @param {ItemStack} item Item to repair
  * @returns {ItemStack} A repaired item
  */
-function repairItem(item: ItemStack): ItemStack {
+export function repairItem(item: ItemStack): ItemStack {
     const newItem = new ItemStack(Items.get(item.id), item.amount, item.data)
     newItem.nameTag = item.nameTag
     newItem.getComponents = item.getComponents
